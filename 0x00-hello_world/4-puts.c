8ca787e5ed82f9601d5adaffdc5611116ebaf7d9
HSKatumba <hskatumba@gmail.com>
2022-06-05 04:25:03 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
 #include<stdio.h>
+/**
+ * using the function puts
+ */
 
 int main(){
 
-	char phase[]="Programming is like building a multilingual puzzle";
-
+	char phase[]="\"Programming is like building a multilingual puzzle";
 	puts(phase);
 	return 0;
 }
