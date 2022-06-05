f6437b3186b7fdb3fb7bb722f7ad7f8b7ed938ca
HSKatumba <hskatumba@gmail.com>
2022-06-05 04:49:29 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,19 +1,20 @@
 #include<stdio.h>
 /*
  * using the function puts
- * */
+ */
 
 int main(void)
-{
+
 	/*
 	 * main function is entry point
-	 * */
-
+	 */
+{
 	char phase[] = "\"Programming is like building a multilingual puzzle";
 
 	puts(phase);
 
-	/* return 0 always 
+	/*
+	 * return 0 always 
 	 * success
 	 */
 	return (0);
