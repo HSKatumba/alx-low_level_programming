0dc02f2fa6b341feea58736e3fdf4ff2c45a2e2f
HSKatumba <hskatumba@gmail.com>
2022-06-05 05:53:02 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,6 @@
-/*
- * This is the description of the main
- */
 #include<stdio.h>
-/* message goes here */
+
 int main(void)
-/**
-*main - Entry point
-*Descroption
-*Retun 0
-*/
 {
 
 	char phase[] = "\"Programming is like building a multilingual puzzle";
