66c53b8e263a67752a1b48f9bb2b7220ffb3ce51
HSKatumba <hskatumba@gmail.com>
2022-06-05 07:02:33 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,8 +9,15 @@
 int main(void)
 {	
 	char chartype;
+	int inttype;
+	long int longtype;
+	double longtype2;
+	float floattype; 
 
-	
 	printf("Size of a char: %zu byte\(s\)\n", sizeof(chartype));
+	printf("Size of an int: %zu byes\(s\)\n", sizeof(inttype));
+	printf("Size of long int: %zu byte\(s\)\n", sizeof(longtype));
+	printf("Size of long long int: %zu bype\(s\)\n", sizeof(longtype2));
+	printf("Size of float: %zu bytes\(s\)\n", sizeof(floattype)); 
 	return (0);
 }
