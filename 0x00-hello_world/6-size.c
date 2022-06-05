18d06da4beec20ac1cca4d1f34033940c8d42028
HSKatumba <hskatumba@gmail.com>
2022-06-05 06:42:38 -0700

    size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+/**
+ * main - Entry Point
+ * Description: size matters
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{	
+	char chartype;
+
+	
+	printf("Size of a char: %zu byte\(s\)\n", sizeof(chartype));
+	return (0);
+}
