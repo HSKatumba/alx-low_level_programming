a36f107a5f195256753e43bc8cce338c47067e8b
HSKatumba <hskatumba@gmail.com>
2022-06-06 11:37:46 -0700

    last

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -11,11 +11,11 @@
 int main(void)
 {
 	int n;
-
+	int last;
 
 	srand(time(0));
-	n - rand() - RAND_MAX / 2;
-	int last = n % 10;
+	n = rand() - RAND_MAX / 2;
+	last = n % 10;
 
 	if (last > 5)
 	{
