291145a269404c2d0a138a0cc72d4f2ff75332b2
HSKatumba <hskatumba@gmail.com>
2022-06-08 13:14:12 -0700

    msg

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+
+/**
+*main - Entry Point
+* Description: Print random
+*Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
