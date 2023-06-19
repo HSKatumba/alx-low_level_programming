4c04394be806748d63ef777aafa2c84a3613bcc3
HSKatumba <hskatumba@gmail.com>
2023-06-19 07:37:32 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+* main - entry point
+*
+*return 0 always
+*/
+int main(void)
+{
+	char message[] = "Programming is like building a multilingual puzzle";
+	puts(message);
+	return(0);
+}
