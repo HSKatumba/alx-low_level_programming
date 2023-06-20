a04b929fbe655363b35715652f20cb97ff0757a9
HSKatumba <hskatumba@gmail.com>
2023-06-20 11:03:05 -0700

    monica

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+*/
+int main(void)
+{
+	char var[] = "_putchar";
+
+	printf("%s\n", var);
+
+	return (0);
+}
